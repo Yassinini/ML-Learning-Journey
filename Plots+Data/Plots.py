b1c8b63d01a30063d15cd1cffa57c75c7da05e28
Yassinini <yassinmehana2009@gmail.com>
2026-03-11 10:21:00 +0300

    New Datasets && Edits on Automation

--- Plots+Data/Plots.py
+++ Plots+Data/Plots.py
@@ -10,47 +10,51 @@ def sort(df, column):
 def Filter(x):
     return x.dropna()
 
-def scatter(d,x,y, outliers=True, percentile=95):
+def scatter(d,x,y, outliers=True, percentile=95, title=""):
     data=Filter(d)
     if outliers and pd.api.types.is_numeric_dtype(data[x]) and pd.api.types.is_numeric_dtype(data[y]):
         data = data[(data[x] < np.percentile(data[x], percentile)) & 
                     (data[y] < np.percentile(data[y], percentile))]
     plt.figure(figsize=(12,8))
     plt.scatter(data[x], data[y], alpha=0.2)
-    plt.title(f"{x} to {y}")
+    plt.title(title if title 
+              else f"{x} to {y}")
     plt.xlabel(x)
     plt.ylabel(y)
     plt.xticks(rotation=45)
     plt.show()
 
-def bar(d,x,y):
+def bar(d,x,y, title=""):
     data=Filter(d)
     plt.figure(figsize=(15,10))
     plt.bar(data[x], data[y])
-    plt.title(f"{x} to {y}")
+    plt.title(title if title 
+              else f"{x} to {y}")
     plt.xlabel(x)
     plt.ylabel(y)
     plt.xticks(rotation=45)
     plt.show()
 
-def line(d, x, y, outliers=True, percentile=95):
+def line(d, x, y, outliers=True, percentile=95 , title=""):
     data=Filter(d)
     if outliers and pd.api.types.is_numeric_dtype(data[x]) and pd.api.types.is_numeric_dtype(data[y]):
         data=data[(data[x] < np.percentile(data[x], percentile)) & 
                     (data[y] < np.percentile(data[y], percentile))]
     plt.figure(figsize=(15,10))
     plt.plot(data[x], data[y])
-    plt.title(f"{x} to {y}")
+    plt.title(title if title 
+              else f"{x} to {y}")
     plt.xlabel(x)
     plt.ylabel(y)
     plt.xticks(rotation=45)
     plt.show()
 
-d=pd.read_csv(r"Machine_Learning\Plots+Data\attachment_25167_House_Rent_Dataset.csv.csv")
-dc=d[(d["Rent"] < 30000) & (d["Size"] < 7000)] 
-sort(dc, "Posted On")
-#scatter(dc, "Size", "Rent")
-#line(dc , "Posted On", "Rent")
+
+path = kagglehub.dataset_download("anassarfraz13/housing-dataset-info-about-houses")
+d = pd.read_csv(path + "/Housing.csv")
+#scatter(d, "area", "price")
+#line(d, "area", "price")
+
 
 path= kagglehub.dataset_download("dmahajanbe23/bmw-global-automotive-sales")
 d=pd.read_csv(path + "/bmw_global_sales_2018_2025.csv")
@@ -63,3 +67,10 @@ d=pd.read_csv(path + "/breast_cancer_risk_factors.csv")
 
 p=pd.read_csv(path + "/breast_cancer_survival_by_stage.csv")
 line(p, "Stage", "One_Year_Survival_Pct")
+line(p, "Stage", "Five_Year_Survival_Pct")
+line(p, "Stage", "Ten_Year_Survival_Pct")
+#line(p, "Stage", "Typical_Treatment")
+
+path = kagglehub.dataset_download("alexisbcook/data-for-datavis")
+l=pd.read_csv(path + "/insurance.csv")
+#scatter(l, "age", "bmi", title="Age vs BMI, Insurance")
